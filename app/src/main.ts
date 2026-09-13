a8a9e5d908d138a6af6ae0d7519867926d04fac6
Agbabinathedev <agbabina5@gmail.com>
2026-09-13 14:54:38 +0100

    A really minor bug, I forgot to change the cors options ifrom frontend to backend

--- app/src/main.ts
+++ app/src/main.ts
@@ -9,9 +9,9 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: 'https://dev-pilot-ashy.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   await app.listen(process.env.PORT ?? 3000);
